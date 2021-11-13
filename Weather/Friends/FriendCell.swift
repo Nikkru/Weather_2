0955d43dc8b30e3b6980c2043da42bd89ccd603c
Nikkru <nkru@narod.ru>
2021-11-13 21:34:53 +0300

    friend Cell

---
+++ Weather/Friends/FriendCell.swift
@@ -0,0 +1,26 @@
+//
+//  FriendCell.swift
+//  Weather
+//
+//  Created by Nikolai Krusser on 13.11.2021.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+    
+    @IBOutlet weak var nameFriendLabel: UILabel!
+    @IBOutlet weak var avaFriendImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
