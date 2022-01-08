08c741d1323a5d8c2bc40abcb98626b0c5c23a28
NikNik <nkru@narod.ru>
2022-01-08 16:17:57 +0300

    start model for realm

--- Weather/Models/Weather.swift
+++ Weather/Models/Weather.swift
@@ -6,18 +6,19 @@
 //
 
 import Foundation
+import RealmSwift
 
-class Weather: Decodable {
+class Weather: Object ,Decodable {
     
-    var date = 0.0
-    var temp = 0.0
-    var pressure = 0.0
-    var humidity = 0
-    var weatherName = ""
-    var weatherIcon = ""
-    var windSpeed = 0.0
-    var windDegrees = 0.0
-    var city = ""
+    @objc dynamic var date = 0.0
+    @objc dynamic var temp = 0.0
+    @objc dynamic var pressure = 0.0
+    @objc dynamic var humidity = 0
+    @objc dynamic var weatherName = ""
+    @objc dynamic var weatherIcon = ""
+    @objc dynamic var windSpeed = 0.0
+    @objc dynamic var windDegrees = 0.0
+    @objc dynamic var city = ""
     
     enum CodingKeys: String, CodingKey {
         case date = "dt"
